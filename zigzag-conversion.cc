2246ed90a5907b857c07d6dd6645ce61b297b086
kobeyang <yg54123@126.com>
2014-09-21 17:37:24 +0800

    update tab to 2 spaces

--- zigzag-conversion.cc
+++ zigzag-conversion.cc
@@ -1,22 +1,22 @@
 class Solution {
-	public:
-		string convert(string s, int nRows) {
-			int n = s.size();
-			vector<vector<char>> matrix(nRows, vector<char>(n, ' '));
-			int index = 0;
-			int col = 0;
-			while(index < n) {
-				for(int i = 0; i < nRows && index < n; i++)
-					matrix[i][col] = s[index++];
-				col++;
-				for(int i = nRows - 2; i > 0 && index < n; i--)
-					matrix[i][col++] = s[index++];
-			}
-			string result;
-			for(int i = 0; i < nRows; i++)
-				for(int j = 0; j < n; j++)
-					if(matrix[i][j] != ' ')
-						result.push_back(matrix[i][j]);
-			return result;
-		}
+  public:
+    string convert(string s, int nRows) {
+    int n = s.size();
+    vector<vector<char>> matrix(nRows, vector<char>(n, ' '));
+    int index = 0;
+    int col = 0;
+    while(index < n) {
+      for(int i = 0; i < nRows && index < n; i++)
+        matrix[i][col] = s[index++];
+        col++;
+      for(int i = nRows - 2; i > 0 && index < n; i--)
+        matrix[i][col++] = s[index++];
+    }
+    string result;
+    for(int i = 0; i < nRows; i++)
+      for(int j = 0; j < n; j++)
+        if(matrix[i][j] != ' ')
+          result.push_back(matrix[i][j]);
+    return result;
+  }
 };
