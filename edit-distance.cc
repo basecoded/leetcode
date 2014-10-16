bb2e62830be0f7a80a6e0be64db783bd75107a7b
kobeyang <yg54123@126.com>
2014-10-16 14:46:20 +0800

    update longest-common-prefix

--- edit-distance.cc
+++ edit-distance.cc
@@ -25,3 +25,29 @@ class Solution {
       return result[row][col];
     }
 };
+
+// O(n) space optimization
+class Solution {
+  public:
+    int minDistance(string word1, string word2) {
+      int len1 = word1.size();
+      int len2 = word2.size();
+      vector<int> f(len2 + 1, 0);
+      for (int i = 0; i <= len2; i++)
+        f[i] = i;
+      int upper_left = 0; // to remember f[i-1][j-1]
+      for (int i = 1; i <= len1; i++) {
+        upper_left = f[0];
+        f[0] = i;
+        for (int j = 1; j <= len2; j++) {
+          int upper = f[j];
+          if (word1[i-1] == word2[j-1])
+            f[j] = upper_left;
+          else
+            f[j] = min(upper_left, min(f[j], f[j-1])) + 1;
+          upper_left = upper;
+        }
+      }
+      return f[len2];
+    }
+};
--- longest-common-prefix.cc
+++ longest-common-prefix.cc
@@ -3,14 +3,12 @@ class Solution {
     string longestCommonPrefix(vector<string> &strs) {
       if (strs.size() == 0)
         return "";
-      string prefix;
-      for (int j = 0; j < strs[0].size(); j++) {
-        for (int i = 1; i < strs.size(); i++) {
-          if (j >= strs[i].size() || strs[i][j] != strs[0][j])
-            return prefix;
+      for (int i = 0; i < strs[0].size(); i++) {
+        for (int j = 1; j < strs.size(); j++) {
+          if (i >= strs[j].size() || strs[j][i] != strs[0][i])
+            return strs[0].substr(0, i);
         }
-        prefix += strs[0][j];
       }
-      return prefix;
+      return strs[0];
     }
 };
