04d3e8fc794db2016e1ee4d78e03cc768299584d
Guang Yang <yg54123@gmail.com>
2016-11-18 14:50:14 -0800

    Update search-in-rotated-sorted-array.cc

--- search-in-rotated-sorted-array.cc
+++ search-in-rotated-sorted-array.cc
@@ -1,23 +1,25 @@
 class Solution {
-  public:
-    int search(int A[], int n, int target) {
-      int start = 0, end = n - 1;
-      while (start <= end) {
-        int mid = start + (end - start) / 2;
-        if (A[mid] == target)
-          return mid;
-        if (A[start] <= A[mid]) { // qianbianyouxu
-          if (A[start] <= target && target < A[mid])
-            end = mid - 1;
-          else
-            start = mid + 1;
-        } else {
-          if (A[mid] < target && target <= A[end])
-            start = mid + 1;
-          else
-            end = mid - 1;
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.size() == 0)
+            return -1;
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[left] <= nums[mid]) { // left is ordered
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            } else {
+                if (target > nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
         }
-      }
-      return -1;
+        return -1;
     }
 };
